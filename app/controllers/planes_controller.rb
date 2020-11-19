4ec8a2c181e5373e24962258c5d691919e404507
Thierry Laporte <laportethierry@yahoo.fr>
2020-11-19 17:52:04 +0100

    card and map index

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -20,8 +20,9 @@ class PlanesController < ApplicationController
       {
         lat: plane.latitude,
         lng: plane.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { plane: plane })
+        infoWindow: render_to_string(partial: "info_window", locals: { plane: plane }),
         # image_url: helpers.asset_url('xxx')
+        plane_id: plane.id
       }
     end
 
